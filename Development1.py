ba89db2c215e0f1c56d3a9b669092d6f2bf31a76
SeanDoherty-Strath <113599995+SeanDoherty-Strath@users.noreply.github.com>
2023-10-14 15:22:05 +0100

    Added 3D Plot view
    And new test data with two clusters

--- Development1.py
+++ Development1.py
@@ -5,13 +5,13 @@ import plotly.express as px
 
 
 # COMPONENTS
-# df = pd.read_csv('C:/Users/sdohe/Documents/4th Year Project/TestData.csv')
 df = pd.read_csv(
-    "C:/Users/sdohe/OneDrive/Documents/HPS_App/PinLocation/Scripts/TestData2.csv"
-)
+    'C:/Users/sdohe/Documents/4th Year Project/Scripts/TestData3.csv')
+# df = pd.read_csv("C:/Users/sdohe/OneDrive/Documents/HPS_App/PinLocation/Scripts/TestData2.csv")
 
 df2 = px.data.medals_long()
-app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])  # always the same
+app = Dash(__name__, external_stylesheets=[
+           dbc.themes.SOLAR])  # always the same
 mytext = dcc.Markdown(
     children="# My Graph",
 )
@@ -19,7 +19,7 @@ mytext = dcc.Markdown(
 
 myGraph = dcc.Graph(figure={})
 dropdown = dcc.Dropdown(
-    options=["Data 1", "Data 2", "Data 3"],
+    options=["Data 1", "Data 2", "Data 3", "Data 1 + 2", "Data 1, 2, + 3"],
     value="Data 1",  # initial value displayed when page first loads
     clearable=False,
 )
@@ -30,7 +30,8 @@ app.layout = dbc.Container([mytext, myGraph, dropdown])
 
 
 @app.callback(
-    Output(myGraph, component_property="figure"),
+    [Output(myGraph, component_property="figure"),
+     Output(mytext, component_property="children")],
     Input(dropdown, component_property="value"),
 )
 # function arguments come from the component property of the Input
@@ -44,7 +45,15 @@ def update_graph(user_input):
     elif user_input == "Data 3":
         fig = px.line(df, x="Date", y="Sensor 3", title="Data 3")
 
-    return fig  # returned objects are assigned to the component property of the Output
+    elif user_input == "Data 1 + 2":
+        fig = px.scatter(df, x="Sensor 1", y="Sensor 2", title="Data 1 + 2")
+
+    elif user_input == "Data 1, 2, + 3":
+        fig = px.scatter_3d(df, x="Sensor 1", y="Sensor 2",
+                            z="Sensor 3", title="Data 1, 2 + 3")
+
+    # returned objects are assigned to the component property of the Output
+    return fig, '#' + user_input
 
 
 # Run app
