025eb7124741f47b65d5b6c4528cdab3df2f4a73
SeanDoherty-Strath <113599995+SeanDoherty-Strath@users.noreply.github.com>
2023-10-16 17:49:25 +0100

    Update Development1.py
    Add two plots on line graph
    Time selection

--- Development1.py
+++ Development1.py
@@ -19,7 +19,8 @@ mytext = dcc.Markdown(
 
 myGraph = dcc.Graph(figure={})
 dropdown = dcc.Dropdown(
-    options=["Data 1", "Data 2", "Data 3", "Data 1 + 2", "Data 1, 2, + 3"],
+    options=["Data 1", "Data 2", "Data 3", "Data 1 + 2",
+             "Data 1 + 2 (scatter)", "Data 1, 2, + 3"],
     value="Data 1",  # initial value displayed when page first loads
     clearable=False,
 )
@@ -30,30 +31,45 @@ app.layout = dbc.Container([mytext, myGraph, dropdown])
 
 
 @app.callback(
-    [Output(myGraph, component_property="figure"),
-     Output(mytext, component_property="children")],
+    [Output(myGraph, component_property="figure")],
     Input(dropdown, component_property="value"),
 )
 # function arguments come from the component property of the Input
 def update_graph(user_input):
     if user_input == "Data 1":
-        fig = px.line(df, x="Date", y="Sensor 1", title="Data 1")
+        fig = px.line(df, x="Date", y="Sensor 1",)
 
     elif user_input == "Data 2":
-        fig = px.line(df, x="Date", y="Sensor 2", title="Data 2")
+        fig = px.line(df, x="Date", y="Sensor 2")
 
     elif user_input == "Data 3":
-        fig = px.line(df, x="Date", y="Sensor 3", title="Data 3")
+        fig = px.line(df, x="Date", y="Sensor 3")
 
     elif user_input == "Data 1 + 2":
-        fig = px.scatter(df, x="Sensor 1", y="Sensor 2", title="Data 1 + 2")
+        fig = px.line(df, x="Date", y=["Sensor 1", "Sensor 2"])
+
+    elif user_input == "Data 1 + 2 (scatter)":
+        fig = px.scatter(df, x="Sensor 1", y="Sensor 2")
 
     elif user_input == "Data 1, 2, + 3":
         fig = px.scatter_3d(df, x="Sensor 1", y="Sensor 2",
-                            z="Sensor 3", title="Data 1, 2 + 3")
+                            z="Sensor 3")
 
     # returned objects are assigned to the component property of the Output
-    return fig, '#' + user_input
+    return fig,
+
+
+@app.callback(
+    Output(mytext, component_property='children'),
+    Input(myGraph, component_property='clickData')
+)
+def display_selected_time(clickData):
+    if clickData is None:
+        return "Click on the graph to display the time."
+
+    selected_time = clickData['points'][0]['x']
+
+    return "# Selected Time: " + selected_time,
 
 
 # Run app
